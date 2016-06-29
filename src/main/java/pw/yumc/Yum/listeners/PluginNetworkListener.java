1c3da1f0105ab6af4bcde67d96d42f9fea108669
502647092 <admin@yumc.pw>
2016-06-29 19:27:00 +0800

    feat: 添加网络忽略新Key 更新pom文件
    Signed-off-by: 502647092 <admin@yumc.pw>

--- src/main/java/pw/yumc/Yum/listeners/PluginNetworkListener.java
+++ src/main/java/pw/yumc/Yum/listeners/PluginNetworkListener.java
@@ -12,10 +12,10 @@ import pw.yumc.Yum.managers.ConfigManager;
 
 public class PluginNetworkListener implements Listener {
     public String prefix = "§6[§bYum §a网络管理§6] ";
-    public String warnMain = "§c插件 §6%s §c尝试在主线程访问网络 §4可能会导致服务器卡顿或无响应!";
-    public String warn = "§c插件 §6%s §c尝试访问网络 §4请注意服务器网络安全!";
-    public String breaked = "§c已阻止插件 §6%s §c访问网络!";
-    public String url = "§c地址: %s";
+    public String warnMain = "§6插件 §b%s §c尝试在主线程访问网络 §4可能会导致服务器卡顿或无响应!";
+    public String warn = "§6插件 §b%s §c尝试访问网络 §4请注意服务器网络安全!";
+    public String breaked = "§c已阻止插件 §b%s §c访问网络!";
+    public String url = "§6地址: §c%s";
 
     public PluginNetworkListener(final Yum yum) {
         Bukkit.getPluginManager().registerEvents(this, yum);
@@ -36,7 +36,7 @@ public class PluginNetworkListener implements Listener {
         if (urlinfo.startsWith("socket")) {
             return;
         }
-        if (urlinfo.contains("yumc")) {
+        if (urlinfo.contains("yumc") || urlinfo.contains("502647092")) {
             final String 大神你好 = "反编译的大神们我知道你们又要说了这货有后门";
             大神你好.isEmpty();
             return;
